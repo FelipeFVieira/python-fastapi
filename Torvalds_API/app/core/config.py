badb68c0fcf40eefd760e263c56994b6d39d1179
Felipe França <felipefrancavieira5@gmail.com>
2024-12-07 02:04:07 -0300

    fix: rename variable

--- Torvalds_API/app/core/config.py
+++ Torvalds_API/app/core/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     sender_email: str
-    secret_password: str
+    sender_password: str
     debug: bool = False
     
     class Config:
