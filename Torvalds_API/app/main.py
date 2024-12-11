09c86a2ed6c94ab4d3dcea16a9ed5403ee3e6703
Felipe França <felipefrancavieira5@gmail.com>
2024-12-10 22:07:26 -0300

    build: Testing another Dockerfile

--- Torvalds_API/app/main.py
+++ Torvalds_API/app/main.py
@@ -11,7 +11,3 @@ app.include_router(router, prefix="/api/v1")
 @app.get("/")
 def root():
     return {"message": "Welcome to Torvalds API!"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
